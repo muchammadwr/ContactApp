dfd5b0606120dd8c650178e7523a991a82a9b9f4
muchammadwr <muchammadwr@gmail.com>
2024-03-14 20:40:38 +0700

    bagian 2

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const { tulisPertanyaan, simpanContact } = require("./contacts");
+const yargs = require("yargs");
 
-const main = async () => {
-  const name = await tulisPertanyaan("Whats your name? ");
-  const phone = await tulisPertanyaan("Whats your phone number? ");
-  const email = await tulisPertanyaan("Whats your email? ");
-  simpanContact(name, phone, email);
-};
+// Mengambil argumen dari command line
+// const command = process.argv;
+// const { tulisPertanyaan, simpanContact } = require("./contacts");
 
-main();
+// const main = async () => {
+//   const name = await tulisPertanyaan("Whats your name? ");
+//   const phone = await tulisPertanyaan("Whats your phone number? ");
+//   const email = await tulisPertanyaan("Whats your email? ");
+//   simpanContact(name, phone, email);
+// };
+
+// main();
+
+console.log(yargs.argv);
